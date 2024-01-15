6f616b57a2e012911120efca8c870d52dee94bf4
Riley Rawson <rileynrawson@gmail.com>
2024-01-14 19:05:28 -0700

    updated equals and hashCode functions. Passing inital tests in all piece classes

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -41,5 +43,36 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         return this.promotionPiece;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        sb.append(startPosition.getRow());
+        sb.append(",");
+        sb.append(startPosition.getColumn());
+        sb.append(")");
+        sb.append("->");
+        sb.append("(");
+        sb.append(endPosition.getRow());
+        sb.append(",");
+        sb.append(endPosition.getColumn());
+        sb.append(")");
+        return sb.toString();
     }
 }
