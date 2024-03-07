36c1c6dc29169d771cb3b09bf1100c8bec5b8ac1
Riley Rawson <rileynrawson@gmail.com>
2024-03-06 23:54:26 -0700

    SQL User & Game DAOs working

---
+++ server/src/main/java/dataAccess/SQLGameDAO.java
@@ -0,0 +1,127 @@
+package dataAccess;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.GameData;
+import model.GameListData;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class SQLGameDAO implements GameDAO{
+    @Override
+    public void clear() throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("DROP TABLE game");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void createGame(GameData data) throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("INSERT INTO game (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?, ?, ?)");
+            statement.setInt(1, data.gameID());
+            statement.setNString(2, data.whiteUsername());
+            statement.setNString(3, data.blackUsername());
+            statement.setNString(4, data.gameName());
+            var json = new Gson().toJson(data.game());
+            statement.setNString(5, json);
+            statement.executeUpdate();
+        } catch (SQLException e) { throw new DataAccessException(e.getMessage());}
+    }
+
+    @Override
+    public GameData getGameByName(String gameName) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("SELECT * FROM game WHERE gameName = ?");
+            statement.setNString(1, gameName);
+            try (var rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    int dbGameID = rs.getInt("gameID");
+                    String dbWhiteUsername = rs.getString("whiteUsername");
+                    String dbBlackUsername = rs.getString("blackUsername");
+                    var json = rs.getString("chessGame");
+                    ChessGame dbGame = new Gson().fromJson(json, ChessGame.class);
+                    return new GameData(dbGameID,dbWhiteUsername, dbBlackUsername, gameName, dbGame);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public GameData getGameByID(int gameID) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("SELECT * FROM game WHERE gameID = ?");
+            statement.setInt(1, gameID);
+            try (var rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    String dbGameName = rs.getString("gameName");
+                    String dbWhiteUsername = rs.getString("whiteUsername");
+                    String dbBlackUsername = rs.getString("blackUsername");
+                    var json = rs.getString("chessGame");
+                    ChessGame dbGame = new Gson().fromJson(json, ChessGame.class);
+
+                    return new GameData(gameID,dbWhiteUsername, dbBlackUsername, dbGameName, dbGame);
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public GameListData listGames() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("SELECT * FROM game");
+            ArrayList<GameData> games = new ArrayList<GameData>(); //list
+            try (var rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    int dbGameID = rs.getInt("gameID");
+                    String dbGameName = rs.getString("gameName");
+                    String dbWhiteUsername = rs.getString("whiteUsername");
+                    String dbBlackUsername = rs.getString("blackUsername");
+                    var json = rs.getString("chessGame");
+                    ChessGame dbGame = new Gson().fromJson(json, ChessGame.class);
+                    games.add(new GameData(dbGameID,dbWhiteUsername, dbBlackUsername, dbGameName, dbGame)); //append to list
+                }
+                return new GameListData(games); //return list as gamelistdata
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+
+    @Override
+    public void updateGame(GameData data) throws DataAccessException {
+        GameData tempData = getGameByID(data.gameID());
+        if (tempData != null) {
+            try {
+                var conn = DatabaseManager.getConnection();
+                var statement = conn.prepareStatement("UPDATE game SET whiteUsername = ?, blackUsername = ?, gameName = ?, chessGame = ? WHERE gameID = ?");
+                statement.setNString(1, data.whiteUsername());
+                statement.setNString(2, data.blackUsername());
+                statement.setNString(3, data.gameName());
+                var json = new Gson().toJson(data.game());
+                statement.setNString(4, json);
+                statement.setInt(5, data.gameID());
+
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage());
+            }
+        }
+        else {
+            throw new DataAccessException("Error: bad request");
+        }
+    }
+}
---
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -0,0 +1,53 @@
+package dataAccess;
+
+import model.AuthData;
+import model.UserData;
+
+import java.sql.SQLException;
+
+public class SQLUserDAO implements UserDAO{
+    @Override
+    public void clear() throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("DROP TABLE user");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void createUser(UserData data) throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)");
+            statement.setNString(1, data.username());
+            statement.setNString(2, data.password());
+            statement.setNString(3, data.email());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) { throw new DataAccessException(e.getMessage());}
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("SELECT * FROM user WHERE username = ?");
+            statement.setNString(1, username);
+            try (var rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    String dbUsername = rs.getString("username");
+                    String dbPassword = rs.getString("password");
+                    String dbEmail = rs.getString("email");
+                    return new UserData(dbUsername, dbPassword, dbEmail);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+}
