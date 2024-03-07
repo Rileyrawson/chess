74e16d99b9facc1a71ee520a59f3857fd5b2932d
Riley Rawson <rileynrawson@gmail.com>
2024-03-06 23:46:40 -0700

    SQL Auth DAO working

---
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -0,0 +1,62 @@
+package dataAccess;
+import model.AuthData;
+import java.sql.SQLException;
+
+
+public class SQLAuthDAO implements AuthDAO{
+    @Override
+    public void clear() throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("DROP TABLE auth");
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void createAuth(AuthData data) throws DataAccessException {
+        try {
+            var conn = DatabaseManager.getConnection();
+            var statement = conn.prepareStatement("INSERT INTO auth (authToken, username) VALUES (?, ?)");
+            statement.setNString(1, data.authToken());
+            statement.setNString(2, data.username());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) { throw new DataAccessException(e.getMessage());}
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("SELECT * FROM auth WHERE authToken = ?");
+            statement.setNString(1, authToken);
+            try (var rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    String dbAuthToken = rs.getString("authToken");
+                    String dbUsername = rs.getString("username");
+                    return new AuthData(dbAuthToken, dbUsername);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(AuthData data) throws DataAccessException {
+        String oldAuthToken = data.authToken();
+
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = conn.prepareStatement("DELETE FROM auth WHERE authToken = ?");
+            statement.setNString(1, oldAuthToken);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+    }
+}
